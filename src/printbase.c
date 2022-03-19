3f059792642d0d3fa115a4d2f24419db7110c5bc
mine <leone@monokuma>
2022-03-19 07:45:30 +0000

    update

--- src/printbase.c
+++ src/printbase.c
@@ -7,7 +7,7 @@ int printbase	 (int		c,
   int i = 0;
   if (c != 0 && (c - c % d) / d != 0)
     i += printbase((c - c % d) / d, d);
-  char j = (char) c % d + '0';
+  char j = (c % d) + '0';
   write (1, &j, 1);
   return i + 1;
 }
